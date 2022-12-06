1df177ad6de56341e091a47dbedf031c7f266fb6
Emily Moore <emilymoore3@nhs.net>
2022-12-06 10:22:59 +0000

    load borders data add age

--- examples.R
+++ examples.R
@@ -6,12 +6,24 @@
 
 #load ggplot
 library(ggplot2)
+library(lubridate)
+library(dplyr)
 
 
 # load data
+borders_data <- readRDS("data/borders.rds")
+
+#change date strings to actual dates and create age on admission variable
+borders_data <- borders_data %>%
+  mutate(Dateofbirth = paste0(substr(Dateofbirth,1,4), "-",substr(Dateofbirth,5,6), "-" ,substr(Dateofbirth,7,8) ))  %>%
+  mutate(DateofAdmission = paste0(substr(DateofAdmission,1,4), "-",substr(DateofAdmission,5,6), "-" ,substr(DateofAdmission,7,8) ))  %>%
+  mutate(ageonadmission = time_length(difftime(as.Date(DateofAdmission, "%Y-%m-%d"),as.Date(Dateofbirth, "%Y-%m-%d") ),"years"))
+
+table(borders_data$ageonadmission)
 
 
-borders_data_plot %>%
+##Example 1 plot####
+borders_data %>%
   ggplot(aes(x = ageonadmission, 
              y = LengthOfStay, 
              colour = Sex)) +
